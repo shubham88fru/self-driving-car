c81cdf963090f841343340eaff2be462a0850aab
shubham88fru <shubham88fru@yahoo.com>
2024-02-19 21:02:22 -0700

    add lanes

--- main.js
+++ main.js
@@ -2,15 +2,20 @@ const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
-const car = new Car(100, 100, 30, 50);
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 
 animate();
 
 function animate() {
   car.update();
   canvas.height = window.innerHeight;
-
+  ctx.save();
+  ctx.translate(0, -car.y + canvas.height * 0.7);
+  road.draw(ctx);
   car.draw(ctx);
+
+  ctx.restore();
   //calls the callback (in this case - animate)
   //many times again and again, many times per second.
   requestAnimationFrame(animate);
